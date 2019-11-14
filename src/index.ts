2565815c483368c62a28c7e67ba00e6c5199c95b
yiran <fengyiran@joycastle.mobi>
2019-11-14 16:59:30 +0800

    :sparkles:  redis session 配置

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import connectRedis from "connect-redis";
 import dotenv from "dotenv";
 import express from "express";
+import expressSession from "express-session";
 import path from "path";
+import redis from "redis";
 import * as routes from "./routers";
+
+const RedisStore = connectRedis(expressSession);
 
 // initialize configuration
 dotenv.config();
@@ -16,6 +21,14 @@ app.use(express.json());
 // Configure Express to use EJS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+const redisClient = redis.createClient(6379, "127.0.0.1");
+app.use(expressSession({
+    resave: false,
+    saveUninitialized: false,
+    secret: "password",
+    store: new RedisStore({ client: redisClient }),
+}));
 
 routes.register(app);
 
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,9 +1,17 @@
-import * as express from "express";
 import * as api from "./api";
 
-export const register = (app: express.Application) => {
+export const register = (app: any) => {
   // define a route handler for the default home page
-  app.get("/", (req, res) => {
+  app.get("/", (req: any, res: any) => {
+
+    if (req.session.site !== undefined) {
+      // tslint:disable-next-line:no-console
+      console.log(req.session.site);
+    }
+
+    // 设置Session
+    req.session.site = {name: "xxx", domain: "bbb"};
+
     // render the index template
     res.render("index");
   });
